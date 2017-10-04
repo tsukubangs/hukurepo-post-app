7c2e796444fe8cb6177ef0a1b1c4b0559a0e7364
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-10-04 14:47:33 +0900

    add scss and can change

--- src/components/CameraPage.vue
+++ src/components/CameraPage.vue
@@ -224,7 +224,9 @@ export default {
 
 </script>
 
-<style scoped >
+<style lang="scss" scoped >
+@import "./../../config.scss";
+
  #text-form {
    width: 100%;
    margin: 10px 0;
@@ -273,7 +275,7 @@ export default {
   bottom: 0;
 }
 .post-problem-btn {
-  background-color: #01a8ec;
+  background-color: $main-color;
   color: #fff;
   padding-left: 15px;
   padding-right: 15px;
