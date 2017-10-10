8cda2c30015232e4e2d601211542509e3e7ebe1f
ShuheiGotou <uhsog.tsukuba@gmail.com>
2017-10-10 16:01:18 +0900

    add .env example

--- .env.example.js
+++ .env.example.js
@@ -2,3 +2,6 @@ export const GOOGLE_MAP_API = "";
 export const WEB_API_URL = '';
 
 export const QUESTIONNAIRE_URL = '';
+
+export const PRESENT_EXCHANGE_PLACE = "";
+export const PRESENT_PASSWORD = "";
