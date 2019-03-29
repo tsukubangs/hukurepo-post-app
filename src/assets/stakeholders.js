fc2a5fab7e2a34696cbc6c8fa9921dff42e2e6ef
Hukuowl <34501660+tsukubangs@users.noreply.github.com>
2019-03-29 14:36:21 +0900

    協賛対応

---
+++ src/assets/stakeholders.js
@@ -0,0 +1,47 @@
+export default [
+                {
+                "stakeholder": "おみやげ・お食事処　神田家",
+                "url_link": "http://www.kandaya.info/",
+                "Position": {
+                    "Latitude": 36.212783,
+                    "Longitude":140.10123
+                    },
+                "Address": "茨城県つくば市筑波722"
+                },
+                {
+                "stakeholder": "ホテルレイクサイドつくば",
+                "url_link": "https://www.lakeside-tsukuba.jp/",
+                "Position": {
+                    "Latitude": 35.966678,
+                    "Longitude": 140.108242
+                    },
+                "Address": "つくば市下岩崎708"
+                },
+                {
+                "stakeholder": "アーバンアウトドアキッチン「バーキン」",
+                "url_link": "https://tsukubabbq.owst.jp/",
+                "Position": {
+                    "Latitude": 36.111636,
+                    "Longitude": 140.119754
+                    },
+                "Address": "つくば市柴崎春風台A36街区4"
+                },
+                {
+                "stakeholder": "㈱筑波山京成ホテル",
+                "url_link": "http://tsukubasan-keiseihotel.co.jp/",
+                "Position": {
+                    "Latitude": 36.218705,
+                    "Longitude": 140.120798
+                    },
+                "Address": "茨城県つくば市筑波1番地"
+                },
+                {
+                "stakeholder": "ホテルニュー梅屋",
+                "url_link": "http://hotel-umeya.jp/",
+                "Position": {
+                    "Latitude": 36.222119,
+                    "Longitude": 140.108642
+                    },
+                "Address": "つくば市谷田部5650"
+                }
+];
